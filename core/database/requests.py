4f48382affc3ca1944550283194da4e943557b08
zzentr <kiselevant0n@mail.ru>
2024-09-13 15:52:49 +1000

    команда для вывода сводки доходов и расходов за определенный период и за все время

--- core/database/requests.py
+++ core/database/requests.py
@@ -1,7 +1,8 @@
 from core.database.models import async_session
 from core.database.models import Expense, Income
 from sqlalchemy import select, func
-from datetime import datetime
+from datetime import datetime, timedelta
+from asyncio import to_thread
 
 
 async def new_income(income: dict, tg_id: str, date: datetime):
@@ -23,3 +24,31 @@ async def get_categories(tg_id: str):
                                             .desc()).limit(4))
         await session.commit()
         return categories
+    
+async def get_finance(tg_id: str, period: str):
+    async with async_session() as session:
+        period_date = await to_thread(calculate_days, period)
+
+        income_query = select(Income).where(Income.date >= period_date.date(), Income.tg_id == tg_id)
+        expense_query = select(Expense).where(Expense.date >= period_date.date(), Expense.tg_id == tg_id)
+
+        income = await session.scalars(income_query)
+        expense = await session.scalars(expense_query)
+
+        return income.all(), expense.all()
+
+def calculate_days(period: str):
+    days_dict = {'D': 1, 'M': 30, 'Y': 365}
+    days = int(period[0]) * days_dict[period[1]]
+    return datetime.now() - timedelta(days=days)
+
+async def get_finance_all(tg_id: str):
+    async with async_session() as session:
+
+        income_query = select(Income).where(Income.tg_id == tg_id)
+        expense_query = select(Expense).where(Expense.tg_id == tg_id)
+
+        income = await session.scalars(income_query)
+        expense = await session.scalars(expense_query)
+        
+        return income.all(), expense.all()
--- core/handlers/states/finance_state.py
+++ core/handlers/states/finance_state.py
@@ -3,6 +3,10 @@ from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
+from re import fullmatch
+from asyncio import to_thread
+
+from core.database import requests as rq
 
 
 class Finance(StatesGroup):
@@ -15,9 +19,53 @@ router = Router()
 @router.message(Command('finance'))
 async def finance_period(message: Message, state: FSMContext):
     await state.set_state(Finance.period_of_time)
-    await message.answer('укажите за какой период отобразить сводку доходов и расходов.\n'
-                         'формат: 1D; M; 1Y где D-день, M-месяц, Y-год.\n например 1M - 1 месяц')
+    await message.answer('укажите за какой период отобразить сводку доходов и расходов в формате:\n\n'
+                         '1D т.е. 1 день\n3M - 3 месяца\n1Y - 1 год\n'
+                         'ввести можно лишь одно значение\n\n'
+                         'или напишите all чтобы получить сводку за все время')
 
 @router.message(Finance.period_of_time)
 async def display_finance(message: Message, state: FSMContext):
     period = message.text
+    if fullmatch(r'^\d+[DMY]$', period) or period == 'all':
+        if period == 'all':
+            income, expense = await rq.get_finance_all(message.from_user.id)
+        else:
+            income, expense = await rq.get_finance(message.from_user.id, period)
+        all_income = await to_thread(amount_of_money, income)
+        all_expense = await to_thread(amount_of_money, expense)
+        categories = await to_thread(top_category, expense)
+        await message.answer('ваша статистика за данный период:\n\n'
+                            f'заработано: {all_income} руб.\n'
+                            f'потрачено: {all_expense} руб.\n\n'
+                            'топ категорий по трате:\n\n'
+                            f'{''.join(f'{category} - {amount} руб.\n' for category, amount in categories.items())}')
+        await state.clear()
+        return
+
+    await message.answer('введите период в правильном формате')
+
+def amount_of_money(finance):
+    all_money = 0
+
+    for el in finance:
+        all_money += el.amount
+
+    return all_money
+
+def top_category(expenses):
+    category = {}
+
+    for expense in expenses:
+        cat = expense.category
+        if category.get(cat) is not None:
+            continue
+        all_money = 0
+        for el in expenses:
+            if el.category == cat:
+                all_money += el.amount
+        category[cat] = all_money
+    
+    sorted_categories = dict(sorted(category.items(), key=lambda item: item[1], reverse=True)[:4])
+
+    return sorted_categories 
