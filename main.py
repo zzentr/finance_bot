83508a6c3a500bac9e4c99a56040d87a12111a62
Anton Kiselev <161993777+zzentr@users.noreply.github.com>
2025-08-31 20:44:38 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
-from asyncio import run
+from asyncio import run, create_task
 import os
 load_dotenv()
 
@@ -12,12 +12,24 @@ from core.database.models import async_main
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 
+async def handle(request):
+    return web.Response(text="Bot is running ✅")
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get("/", handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    port = int(os.environ.get("PORT", 10000))
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
 
 async def main():
     dp.include_routers(commands.router,
                        expense_state.router, income_state.router, finance_state.router, 
                        text.router)
     await async_main()
+    create_task(start_web_server())
     await dp.start_polling(bot)
 
 
